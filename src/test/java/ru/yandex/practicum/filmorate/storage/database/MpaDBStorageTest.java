90a2782db6eb3138277eca01070a2b29d5408ed5
Kekbaber <Kokmes45@gmail.com>
2026-06-02 12:04:23 +0300

    test(storage): add database integration tests

---
+++ src/test/java/ru/yandex/practicum/filmorate/storage/database/MpaDBStorageTest.java
@@ -0,0 +1,73 @@
+package ru.yandex.practicum.filmorate.storage.database;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.JdbcTemplate;
+import ru.yandex.practicum.filmorate.model.Mpa;
+import ru.yandex.practicum.filmorate.storage.MpaStorage;
+import ru.yandex.practicum.filmorate.storage.db.MpaDBStorage;
+import ru.yandex.practicum.filmorate.storage.db.mappers.MpaRowMapper;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JdbcTest
+@AutoConfigureTestDatabase
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+@Import({MpaDBStorage.class, MpaRowMapper.class})
+class MpaDBStorageTest {
+
+    @Autowired
+    private final MpaStorage mpaStorage;
+
+    @Autowired
+    private final JdbcTemplate jdbc;
+
+    @Test
+    void findAll_ShouldReturnAllMpa() {
+        Collection<Mpa> mpaCollection = mpaStorage.findAll();
+        assertThat(mpaCollection).hasSize(5);
+    }
+
+    @Test
+    void findAll_ShouldReturnCorrectMpaNames() {
+        Collection<Mpa> mpaCollection = mpaStorage.findAll();
+        List<String> names = mpaCollection.stream()
+                .map(Mpa::getName)
+                .toList();
+        assertThat(names).containsExactlyInAnyOrder("G", "PG", "PG-13", "R", "NC-17");
+    }
+
+    @Test
+    void findById_WhenExists_ShouldReturnMpa() {
+        Optional<Mpa> mpa = mpaStorage.findById(1);
+        assertThat(mpa).isPresent();
+        assertThat(mpa.get().getId()).isEqualTo(1);
+        assertThat(mpa.get().getName()).isEqualTo("G");
+    }
+
+    @Test
+    void findById_WhenExists_ShouldReturnCorrectMpaForEachId() {
+        for (int i = 1; i <= 5; i++) {
+            Optional<Mpa> mpa = mpaStorage.findById(i);
+            assertThat(mpa).isPresent();
+            assertThat(mpa.get().getId()).isEqualTo(i);
+        }
+    }
+
+    @Test
+    void findById_WhenNotExists_ShouldReturnEmpty() {
+        Optional<Mpa> mpa = mpaStorage.findById(999L);
+        assertThat(mpa).isEmpty();
+
+        Optional<Mpa> negativeId = mpaStorage.findById(-1L);
+        assertThat(negativeId).isEmpty();
+    }
+}
