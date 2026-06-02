aa625c2406788bff27579db541b0bf00f99218c0
Kekbaber <Kokmes45@gmail.com>
2026-06-02 11:14:17 +0300

    refactor(model): remove validation from model classes because validation is now handled by DTOs

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,30 +1,14 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.*;
 import lombok.Data;
-import ru.yandex.practicum.filmorate.validation.group.OnUpdate;
 
 import java.time.LocalDate;
 
 @Data
 public class User {
-
-    @NotNull(groups = {OnUpdate.class}, message = "Id должен быть указан")
     private Long id;
-
-    @Email(message = "Электронная почта не может быть пустой и должна содержать символ @")
     private String email;
-
-    @NotBlank(message = "Логин не может быть пустым")
-    @Pattern(regexp = "^\\S+$", message = "Логин не должен содержать пробелы")
     private String login;
-
     private String name;
-
-    @Past(message = "Дата рождения не может быть в будущем")
     private LocalDate birthday;
-
-    public String getName() {
-        return (name == null || name.isBlank()) ? login : name;
-    }
 }
