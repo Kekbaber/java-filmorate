6d1a72577f20aa88907a794b1fe3ecba81a967e5
Kekbaber <Kokmes45@gmail.com>
2026-06-03 19:13:56 +0300

    checkstyle: remove unused import from UserDBStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/db/UserDBStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/db/UserDBStorage.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
